a6d247d7f10b7987ee082957141872217d1975d7
siraj <siru9842@gmail.com>
2023-04-26 09:55:37 +0530

    restuarant list json server

---
+++ index.js
@@ -0,0 +1,26 @@
+// import json server
+const jsonServer = require('json-server')
+
+//  create a json server app
+const server = jsonServer.create()
+
+//  set up path for db.json
+const router = jsonServer.router("db.json")
+
+// Returns middlewares used by json server
+const middleware = jsonServer.defaults()
+
+// set up port
+const port = process.env.PORT || 4000
+
+// user router, middleware in server
+server.use(middleware)
+server.use(router)
+
+// app listen
+server.listen(port,()=>{
+    console.log(`json`);
+})
+
+
+
